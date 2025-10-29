e786a424db10ac65b8bfceaf3e4d64172c254ace
leonardo moretti <leonardonabarro2003@gmail.com>
2025-10-28 22:42:33 -0300

    test: add pedido service test

---
+++ src/test/java/PedidoServiceTest.java
@@ -0,0 +1,46 @@
+import org.example.exception.ClienteNaoEncontradoException;
+import org.example.exception.PedidoInvalidoException;
+import org.example.model.Cliente;
+import org.example.repositorys.ClienteRepository;
+import org.example.repositorys.PedidoRepository;
+import org.example.repositorys.ProdutoRepository;
+import org.example.service.PedidoService;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.assertThrows;
+
+public class PedidoServiceTest {
+
+    @Test
+    public void deveLancarExcecaoClienteNaoEncontrado() {
+        ClienteRepository clienteRepo = new ClienteRepository();
+        ProdutoRepository produtoRepo = new ProdutoRepository();
+        PedidoRepository pedidoRepo = new PedidoRepository();
+
+        PedidoService service = new PedidoService(clienteRepo, produtoRepo, pedidoRepo);
+        Scanner scanner = new Scanner("0"); // simula input do console
+
+        assertThrows(ClienteNaoEncontradoException.class, () -> {
+            service.criarPedido("nao@existe.com", scanner);
+        });
+    }
+
+    @Test
+    public void deveLancarExcecaoPedidoVazio() {
+        ClienteRepository clienteRepo = new ClienteRepository();
+        ProdutoRepository produtoRepo = new ProdutoRepository();
+        PedidoRepository pedidoRepo = new PedidoRepository();
+
+        Cliente c = new Cliente("João", "joao@email.com");
+        clienteRepo.adicionar(c);
+
+        PedidoService service = new PedidoService(clienteRepo, produtoRepo, pedidoRepo);
+        Scanner scanner = new Scanner("0\n"); // adiciona quebra de linha
+
+        assertThrows(PedidoInvalidoException.class, () -> {
+            service.criarPedido("joao@email.com", scanner);
+        });
+    }
+}
