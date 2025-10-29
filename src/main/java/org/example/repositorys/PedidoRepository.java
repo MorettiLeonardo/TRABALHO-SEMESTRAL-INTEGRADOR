879f6cc23c70ed506433c220efe32b46d16953fe
leonardo moretti <leonardonabarro2003@gmail.com>
2025-10-28 22:38:02 -0300

    feat: add repositories

---
+++ src/main/java/org/example/repositorys/PedidoRepository.java
@@ -0,0 +1,29 @@
+package org.example.repositorys;
+
+
+import org.example.model.Pedido;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PedidoRepository {
+    private final List<Pedido> pedidos = new ArrayList<>();
+
+    public void adicionar(Pedido pedido) {
+        pedidos.add(pedido);
+    }
+
+    public List<Pedido> listar() {
+        return Collections.unmodifiableList(pedidos);
+    }
+
+    public String serializar() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[PEDIDOS]\n");
+        for (Pedido p : pedidos) {
+            sb.append(p.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
